629e16721b6901fd6125b9821651e92a0301080f
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-13 23:28:35 +0200

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        @lru_cache(maxsize=None)
+        def dp(n):
+            if n<=3:
+                return n
+            return dp(n-1) + dp(n-2) 
+        
+        return dp(n)
+                
