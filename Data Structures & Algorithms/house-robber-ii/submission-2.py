20bdcaff458165470457327b6d1714f73a18c5c6
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-16 13:03:35 +0200

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        if len(nums) == 1:
+            return nums[0]
+
+        @lru_cache()
+        def dp(i, r):
+            if i >= r:
+                return 0
+            return max(dp(i+1, r), dp(i+2, r) + nums[i])
+
+        return max(dp(0, len(nums)-1), dp(1, len(nums)))
