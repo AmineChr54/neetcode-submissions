de2fbd2d96357decbca9ae6f17c949747cd4c301
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-17 14:23:56 +0200

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+
+        @lru_cache(maxsize=None)
+        def dp(s, i):
+            if i >= len(s):
+                return 1
+            if s[i] == '0':
+                return 0
+            if s[i] == '1':
+                if i == len(s) -1:
+                    return 1
+                else:
+                    return dp(s, i+2) + dp(s, i+1)
+            if s[i] == '2':
+                if i == len(s) -1:
+                    return 1
+                else:
+                    if s[i+1] <= '6':
+                        return dp(s, i+2) + dp(s, i+1)
+                    else:
+                        dp(s, i+1)
+            return dp(s, i+1)
+
+        return dp(s, 0)
