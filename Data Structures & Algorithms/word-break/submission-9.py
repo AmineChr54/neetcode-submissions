00f870f38da64214036ef657261086f0b4bfe0ba
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-20 17:54:22 +0200

    Add: word-break - submission-9

---
+++ Data Structures & Algorithms/word-break/submission-9.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict)
+        
+        @lru_cache(maxsize=None)
+        def dp(i):
+            if i == len(s):   
+                return True
+            for j in range(i, len(s)):
+                if (s[i:j+1] in wordDict) and dp(j+1):
+                    return True
+            return False
+        
+        return dp(0)
