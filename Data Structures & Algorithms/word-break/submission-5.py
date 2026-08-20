7910e61e835c630032f1a654c850e0be38896878
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-20 17:49:43 +0200

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        result = False
+        @lru_cache(maxsize=None)
+        def dp(i):
+            if i == len(s):   
+                return True
+            for j in range(i, len(s)):
+                if (s[i:j+1] in wordDict) and dp(j+1):
+                    nonlocal result
+                    result = True
+                    return True
+
+        dp(0)
+        return result
