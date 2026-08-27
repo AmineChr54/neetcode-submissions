eecd60ca82da23697522d849ad022ab9d2245d14
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-27 11:41:41 +0200

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        parent = {i:i for i in range(n)} # each node is its own parent
+
+        def union(u,v):
+            parent[find(v)] = find(u)
+        
+        def find(u):
+            cur = u
+            while parent[cur] != cur:
+                cur = parent[cur]
+            return cur
+
+        for u,v in edges:
+            if find(v) == find(u):
+                return False
+            union(u,v)
+
+        root = find(0)
+        return all(find(i) == root for i in range(1,n))
+            
