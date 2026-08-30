7a87c42816e8c26430baad6d105640e8e7c9342b
Amine Cheikhrouhou <139517320+AmineChr54@users.noreply.github.com>
2026-08-30 12:20:18 +0200

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,20 @@
+SELECT c.customer_id, c.customer_name
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.product_name = 'A'
+
+INTERSECT
+
+SELECT c.customer_id, c.customer_name
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.product_name = 'B'
+
+EXCEPT
+
+SELECT c.customer_id, c.customer_name
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.product_name = 'C'
+
+ORDER BY customer_name;
